a532a52d339f38297a1fe05787aa0d5040cd9361
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2022-04-16 15:12:57 +0100

    No double quotes

--- pages/product/[permalink].js
+++ pages/product/[permalink].js
@@ -158,7 +158,7 @@ export const getStaticPaths = async () => {
 export async function getStaticProps({params: {permalink}}) {
   return {
     props: {
-      page: permalink.split('-').map(item => item.charAt(0).toUpperCase() + item.slice(1)).join(" "),
+      page: permalink.split('-').map(item => item.charAt(0).toUpperCase() + item.slice(1)).join(' '),
       category: 'PDP'
     }, // is passed up to the custom app as pageProps
   }
